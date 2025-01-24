565b8b8a0fb8ddb5efc07453d2c9a2701d190600
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-01-23 21:20:51 -0700

    Seems to work, and is actually a binarySearch, now.

--- code.js
+++ code.js
@@ -1,23 +1,42 @@
 function binarySearch(list, element) {
-    // Find the middle index of the list.
-    var middle = list.length // 2
-    // If the value at the middle index equals the searched value...
-    if(list[middle] == element)
+    // Initialize left and right index to lower and highest index of list.
+    // Result is initialized to -1, as if element is not found, -1 is returned.
+    var leftIndex = 0;
+    var rightIndex = list.length - 1;
+    var resultIndex = -1;
+    
+    // Continue checking indexes in passed list, until positional indexes swap
+    // positions.
+    while(leftIndex <= rightIndex)
     {
-        // return that index.
-        return middle;
+        // Set the index to be check to the mid point between the left and
+        // right indexes.
+        var checkIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+        // If the element we're in search of is found at the mid point...
+        if(list[checkIndex] == element)
+        {
+            // set the resultIndex to the index just checked.
+            resultIndex = checkIndex
+            // Check to the left, to ensure resultIndex is first instance
+            // of searched for element in list.
+            rightIndex = checkIndex - 1;
+        }
+        // If the element we're in search of is greater than the value at the
+        // mid point...
+        else if(list[checkIndex] < element)
+        {
+            // change the left index to search to the right of the mid point.
+            leftIndex = checkIndex + 1;
+        }
+        // If the element we're in search of is less than the value at the mid
+        // point...
+        else
+        {
+            // change the right index to search to the left of the mid point.
+            rightIndex = checkIndex - 1;
+        }
     }
-    // If the value at the middle index is greater than the searched value...
-    else if(list[middle] > element)
-    {
-        // return -1, as we're working with a sorted list, thus the searched
-        // value doesn't exist in the list.
-        return -1;
-    }
-    // If the value at the middle index is less than searched value...
-    else
-    {
-        // recursively search slice of list composed of subsequent index values.
-        return binarySearch(list.slice(middle + 1, list.length), element);
-    }
+    // If rightIndex exceeds leftIndex we've
+    return resultIndex;
 }
